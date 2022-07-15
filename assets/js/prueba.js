dfa153208296c702e9d52de85ccc0378cb104229
nela26 <marige261092@gmail.com>
2022-07-15 13:05:11 -0400

    js1 desafio completado

---
+++ assets/js/prueba.js
@@ -0,0 +1,11 @@
+function get() {
+    /*declaracion inicial */
+    /*llamando a la variable en js*/
+    let quantity = document.querySelector('#quantity').value;
+    let price = document.getElementById('price').value;
+    /*operacion*/
+    total.innerHTML = '' + quantity * price;
+    /*validacion de selector de color*/
+    textfinal.style.backgroundColor = text0.value
+    quantity1.innerHTML = '' + quantity;
+}
